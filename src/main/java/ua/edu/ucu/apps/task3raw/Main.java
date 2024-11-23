2fa4a95cc86f8c56e2fd8a66b3b713b0b3b25d7e
Sofia Sampara <sampara.pn@ucu.edu.ua>
2024-11-23 10:53:04 +0200

    Implemented ProxyImage

---
+++ src/main/java/ua/edu/ucu/apps/task3raw/Main.java
@@ -0,0 +1,9 @@
+package ua.edu.ucu.apps.task3raw;
+
+public class Main {
+    public static void main(String[] args) {
+        RealImage image = new RealImage("C:\\Users\\admin\\Pictures\\Фоновые изображения рабочего стола\\4. Кот с наушниками.jpg");
+        //пробачте за російську, це дефолтні зображення віндовса((
+        image.display();
+    }
+}
---
+++ src/main/java/ua/edu/ucu/apps/task3raw/MyImage.java
@@ -0,0 +1,5 @@
+package ua.edu.ucu.apps.task3raw;
+
+public interface MyImage {
+    void display();
+}
